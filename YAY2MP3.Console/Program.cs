70c7e8fd183d44ea3d42c1424ffd181dda41ed29
Dayveed117 <david.bugalho@ubi.pt>
2023-03-15 03:26:42 +0000

    Further exploration of YoutubeDLSharp

--- YAY2MP3.Console/Program.cs
+++ YAY2MP3.Console/Program.cs
@@ -3,17 +3,25 @@ using YoutubeDLSharp;
 using JsonFormatterPlus;
 using System.Text.RegularExpressions;
 
-var url = "https://www.youtube.com/watch?v=_p6CMcJwtgk";
-var url2 = "https://www.youtube.com/watch?v=bsm271h5gIc";
-var url3 = "https://www.youtube.com/watch?v=JdLCEwEFCMU";
-var url4 = "https://www.youtube.com/playlist?list=PLaoCst0DYyP1E7ZtqPGU26RYG7cd5eAr2";
-
 var ytdl = new YoutubeDL
 {
     YoutubeDLPath = "yt-dlp.exe",
     FFmpegPath = "ffmpeg.exe",
     OutputFolder = KnownFolders.Downloads.Path
 };
+
+// FFmpeg and YoutubeDL executables must be present in our resources
+if (!File.Exists(ytdl.FFmpegPath))
+{
+    Console.WriteLine("Downloading ffmpeg.exe to folder...");
+    await YoutubeDL.DownloadFFmpegBinary();
+}
+
+if (!File.Exists(ytdl.YoutubeDLPath))
+{
+    Console.WriteLine("Downloading yt-dlp.exe to folder...");
+    await YoutubeDL.DownloadYtDlpBinary();
+}
 
 /* 
  * Relevant attributes for downloading a youtube video:
@@ -35,20 +43,40 @@ var ytdl = new YoutubeDL
  * 6. Use persistance to store operation data and progress
  */
 
-Console.WriteLine(File.Exists(ytdl.FFmpegPath));
-Console.WriteLine(File.Exists(ytdl.FFmpegPath));
+// Normal videos
+var url = "https://www.youtube.com/watch?v=_p6CMcJwtgk";
+var url2 = "https://www.youtube.com/watch?v=bCQb-m0RaG0";
 
-//JsonFormatter.Format(JsonConvert.SerializeObject( object )));
-var res = isYoutubeLink(url) ? await ytdl.RunVideoDataFetch(url) : null;
-var res2 = isYoutubeLink(url) ? await ytdl.RunVideoDataFetch(url2) : null;
-var res3 = isYoutubeLink(url) ? await ytdl.RunVideoDataFetch(url3) : null;
-var res4 = isYoutubeLink(url) ? await ytdl.RunVideoDataFetch(url4) : null;
+// Very old video
+var url3 = "https://www.youtube.com/watch?v=JdLCEwEFCMU";
 
-//debug(res);
-//debug(res2);
-//debug(res3);
+// Private playlist
+var url4 = "https://www.youtube.com/playlist?list=PLaoCst0DYyP1E7ZtqPGU26RYG7cd5eAr2";
 
-debug(res4);
+// Public playlist
+var url5 = "https://www.youtube.com/playlist?list=PLzSjbEiFKZ_w9zWXjVSTLi5FUlcPHwQCc";
+
+// Livestream
+var url6 = "https://www.youtube.com/watch?v=jfKfPfyJRdk";
+
+// Shorts
+var url7 = "https://www.youtube.com/shorts/eGIE2tNcuWQ";
+
+var video1 = isYoutubeLink(url) ? await ytdl.RunVideoDataFetch(url) : null;
+var video2 = isYoutubeLink(url2) ? await ytdl.RunVideoDataFetch(url2) : null;
+var old_video = isYoutubeLink(url3) ? await ytdl.RunVideoDataFetch(url3) : null;
+var prv_playlist = isYoutubeLink(url4) ? await ytdl.RunVideoDataFetch(url4) : null;
+var pub_playlist = isYoutubeLink(url5) ? await ytdl.RunVideoDataFetch(url5) : null;
+var livestream = isYoutubeLink(url6) ? await ytdl.RunVideoDataFetch(url6) : null;
+var ytshort = isYoutubeLink(url7) ? await ytdl.RunVideoDataFetch(url7) : null;
+
+debug(video1);
+debug(video2);
+debug(old_video);
+debug(prv_playlist);
+debug(pub_playlist);
+debug(livestream);
+debug(ytshort);
 
 static bool isYoutubeLink(string link)
 {
@@ -58,18 +86,22 @@ static bool isYoutubeLink(string link)
 
 static void debug(RunResult<YoutubeDLSharp.Metadata.VideoData>? res)
 {
-    if (res is null)
+    Console.WriteLine("===========================================");
+
+    if (res is null || !res.Success)
     {
         Console.WriteLine("Link is not valid.");
         return;
     }
 
-    Console.WriteLine("===========================================");
-    foreach (var item in res.Data.Formats)
-        Console.WriteLine(item);
+    if (res.Data.IsLive ?? false)
+    {
+        Console.WriteLine("Link is a livestream...");
+        return;
+    }
 
-    foreach (var item in res.Data.Thumbnails)
-        Console.WriteLine($"{item.Preference} {item.Resolution}");
+    Console.WriteLine(res.Data.Title);
+    Console.WriteLine(res.Data.ResultType);
 }
 
 // await ytdl.RunAudioDownload("https://www.youtube.com/watch?v=OozUwqWbXzc", AudioConversionFormat.Mp3);
